e71361274083f7567065ae9f6c94c8879a0d89d8
Tom Boddaert <70723965+tomBoddaert@users.noreply.github.com>
2024-04-24 20:05:05 +0100

    Added as mutable reference functions for `Any`

--- lib/any/mod.rs
+++ lib/any/mod.rs
@@ -21,22 +21,36 @@ pub enum Any<'json, 'p> {
 macro_rules! as_impl {
     (
         $variant:pat => $type:ty [$type_name:literal] $value:ident:
-        $as:ident, $as_or:ident, $as_or_else:ident
+        $as:ident, $as_or:ident, $as_or_else:ident,
+        $mut_as:ident, $mut_as_or:ident, $mut_as_or_else:ident
     ) => {
         as_impl! {
             $variant => $type ["a", $type_name] $value:
-            $as, $as_or, $as_or_else
+            $as, $as_or, $as_or_else,
+            $mut_as, $mut_as_or, $mut_as_or_else
         }
     };
 
     (
         $variant:pat => $type:ty [$a:literal, $type_name:literal] $value:ident:
-        $as:ident, $as_or:ident, $as_or_else:ident
+        $as:ident, $as_or:ident, $as_or_else:ident,
+        $mut_as:ident, $mut_as_or:ident, $mut_as_or_else:ident
     ) => {
         #[inline]
         #[must_use]
         #[doc = concat!("Get the value as ", $a, " [`", $type_name, "`].")]
         pub const fn $as(self) -> Option<$type> {
+            if let $variant = self {
+                Some($value)
+            } else {
+                None
+            }
+        }
+
+        #[inline]
+        #[must_use]
+        #[doc = concat!("Get the value as ", $a, " [`&mut ", $type_name, "`].")]
+        pub fn $mut_as(&mut self) -> Option<&mut $type> {
             if let $variant = self {
                 Some($value)
             } else {
@@ -64,11 +78,46 @@ macro_rules! as_impl {
         }
 
         #[inline]
+        #[doc = concat!("Try to get the value as ", $a, " [`&mut ", $type_name, "`].")]
+        ///
+        #[doc = concat!(
+            "Arguments passed to `", stringify!($as_or), "` are eagerly evaluated; ",
+            "if you are passing the result of a function call, it is recommended to ",
+            "use [`Self::", stringify!($as_or_else), "`], which is lazily evaluated."
+        )]
+        ///
+        /// # Errors
+        #[doc = concat!("Returns [`Err(err)`](Err) if `self` is not ", $a, " [`", $type_name, "`]")]
+        pub fn $mut_as_or<E>(&mut self, err: E) -> Result<&mut $type, E> {
+            if let $variant = self {
+                Ok($value)
+            } else {
+                Err(err)
+            }
+        }
+
+        #[inline]
         #[doc = concat!("Try to get the value as ", $a, " [`", $type_name, "`].")]
         ///
         /// # Errors
         #[doc = concat!("Returns [`Err(err())`](Err) if `self` is not ", $a, " [`", $type_name, "`]")]
         pub fn $as_or_else<E, F>(self, err: F) -> Result<$type, E>
+        where
+            F: FnOnce() -> E,
+        {
+            if let $variant = self {
+                Ok($value)
+            } else {
+                Err(err())
+            }
+        }
+
+        #[inline]
+        #[doc = concat!("Try to get the value as ", $a, " [`&mut ", $type_name, "`].")]
+        ///
+        /// # Errors
+        #[doc = concat!("Returns [`Err(err())`](Err) if `self` is not ", $a, " [`", $type_name, "`]")]
+        pub fn $mut_as_or_else<E, F>(&mut self, err: F) -> Result<&mut $type, E>
         where
             F: FnOnce() -> E,
         {
@@ -101,26 +150,31 @@ impl<'json, 'p> Any<'json, 'p> {
 
     as_impl! {
         Self::String(value) => String<'json, 'p> ["String"] value:
-        string, string_or, string_or_else
+        string, string_or, string_or_else,
+        mut_string, mut_string_or, mut_string_or_else
     }
 
     as_impl! {
         Self::Number(value) => Number<'json, 'p> ["Number"] value:
-        number, number_or, number_or_else
+        number, number_or, number_or_else,
+        mut_number, mut_number_or, mut_number_or_else
     }
 
     as_impl! {
         Self::Object(value) => Object<'json, 'p> ["an", "Object"] value:
-        object, object_or, object_or_else
+        object, object_or, object_or_else,
+        mut_object, mut_object_or, mut_object_or_else
     }
 
     as_impl! {
         Self::Array(value) => Array<'json, 'p> ["an", "Array"] value:
-        array, array_or, array_or_else
+        array, array_or, array_or_else,
+        mut_array, mut_array_or, mut_array_or_else
     }
 
     as_impl! {
         Self::Literal(value) => Literal<'json, 'p> ["Literal"] value:
-        literal, literal_or, literal_or_else
+        literal, literal_or, literal_or_else,
+        mut_literal, mut_literal_or, mut_literal_or_else
     }
 }
